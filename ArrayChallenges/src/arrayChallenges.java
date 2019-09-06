4404b33e39e7b378eb960e9e1f016adb8d996d71
emily.deline <emily.deline@MU-C1-04.mullen.pvt.k12.co.us>
2019-09-06 11:55:44 -0600

    Commit Two

--- ArrayChallenges/src/arrayChallenges.java
+++ ArrayChallenges/src/arrayChallenges.java
@@ -5,9 +5,9 @@ public class arrayChallenges
 
 		public static void main(String[] args)
 			{
-				sweet();
-				guessRandomNumber();
-		
+				//fiveRandomNumbers();
+				//sweet();
+				eigthNumbers();
 			}
 
 		private static void sweet()
@@ -17,32 +17,66 @@ public class arrayChallenges
 				int numberOfSweets = userInput.nextInt();
 				System.out.println("COOL! " + numberOfSweets + " is a good number to have!");
 
-				System.out.println("Please enter the name of your favorite sweets one at a time.");	
-				
-				String [] favSweets = new String[numberOfSweets];
+				String[] favSweets = new String[numberOfSweets];
 				Scanner sweetInput = new Scanner(System.in);
 
 				for (int i = 0; i < numberOfSweets; i++)
 					{
-						System.out.println("Please enter your next sweet.");
+						System.out.println("Please enter a sweet.");
 
 						String sweet = sweetInput.nextLine();
-						
+
 						favSweets[i] = sweet;
-						
+
 					}
 
-				
-				
-					
-					
-				
+				for (String s : favSweets)
+					{
+						System.out.println(s);
+					}
+
 			}
-	
-		private static void guessRandomNumber()
+
+		private static void eigthNumbers()
+			{
+				System.out.println("Please enter one of eigth numbers");
+				
+				Scanner intInput = new Scanner(System.in);
+				
+				int userNumbers = intInput.nextInt();
+				
+				int [] eightNumbers = new int[8];
+				
+				for (int i = 0; i < 7; i++)
+					{
+						System.out.println("Enter your next number");
+						
+						eightNumbers[i] = intInput.nextInt();
+					}
+				
+				int evenSum = 0;
+				int oddSum = 0;
+				
+				if (userNumbers % 2 == 0)
+					{
+						evenSum += userNumbers;
+					}
+				else  
+					{
+						oddSum += userNumbers;
+					}
+				System.out.println("The sum of all your even numbers is: " + evenSum);
+				System.out.println("The sum of all your even numbers is: " + oddSum);
+
+
+
+			}
+		
+
+		private static void fiveRandomNumbers()
 			{
 				
-							
 			}
+
 
 	}
